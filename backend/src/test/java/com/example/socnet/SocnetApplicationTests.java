c48d5d0bd5cc68181b128291c2cfbbecac81d5a9
A-N-uraag <marapallyanuraag@gmail.com>
2022-04-06 01:37:54 +0530

    Initialized React and Spring Boot projects

---
+++ backend/src/test/java/com/example/socnet/SocnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
