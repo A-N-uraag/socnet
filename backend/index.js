cc6fa0bf55c9e92fc15bb5ab3a928e68714d05fe
Khalid6468 <khalimohammed5@gmail.com>
2022-04-28 02:12:59 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routesHandler = require('./routes/handler.js')
+const postsHandler = require('./routes/posts.js')
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', routesHandler);
+
+
+app.get('/posts', postsHandler.getAllPosts);
+app.post('/post', postsHandler.postContent);
+app.get('/post/:pid', postsHandler.getPost);
+app.post('/post/:pid/like', postsHandler.likePost);
+app.post('/post/:pid/unlike', postsHandler.unlikePost);
+app.post('/post/:pid/comment', postsHandler.commentPost);
+app.post('/post/:pid/repost', postHandler.repostPost);
+app.post('/post/:pid/report', postHandler.reportPost);
+app.delete('/post/:pid', postHandler.deletePost);
+
+
 
 const PORT = 4000;
 
