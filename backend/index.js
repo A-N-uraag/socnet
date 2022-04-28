71b1e0fc12821e7ee59cbb35bddb0942ae3eea60
A-N-uraag <marapallyanuraag@gmail.com>
2022-04-28 06:00:05 +0530

    Commented the app routes as they are resulting in errors

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.get('/posts', postsHandler.getAllPosts);
-app.post('/post', postsHandler.postContent);
-app.get('/post/:pid', postsHandler.getPost);
-app.post('/post/:pid/like', postsHandler.likePost);
-app.post('/post/:pid/unlike', postsHandler.unlikePost);
-app.post('/post/:pid/comment', postsHandler.commentPost);
-app.post('/post/:pid/repost', postHandler.repostPost);
-app.post('/post/:pid/report', postHandler.reportPost);
-app.delete('/post/:pid', postHandler.deletePost);
+// app.get('/posts', postsHandler.getAllPosts);
+// app.post('/post', postsHandler.postContent);
+// app.get('/post/:pid', postsHandler.getPost);
+// app.post('/post/:pid/like', postsHandler.likePost);
+// app.post('/post/:pid/unlike', postsHandler.unlikePost);
+// app.post('/post/:pid/comment', postsHandler.commentPost);
+// app.post('/post/:pid/repost', postHandler.repostPost);
+// app.post('/post/:pid/report', postHandler.reportPost);
+// app.delete('/post/:pid/delete', postHandler.deletePost);
 
-
+app.get('/', (req, res) => {
+    res.send('root')
+})
 
 const PORT = 4000;
 
