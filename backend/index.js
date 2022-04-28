5b6c82d90046c2790e6857520870671ae7a3b3a3
A-N-uraag <marapallyanuraag@gmail.com>
2022-04-28 06:10:35 +0530

    Updated Port

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.send('root')
 })
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
