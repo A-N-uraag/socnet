09e86fb369e14e075a9add96d3d37ebab1489c63
Sowrya Gali <galisowrya@gmail.com>
2022-04-30 00:16:19 +0530

    Akash Changes

--- frontend/src/components/homepage/homePage.tsx
+++ frontend/src/components/homepage/homePage.tsx
@@ -4,50 +4,53 @@ import { auth } from "../../firebase/firebase";
 import FeedComponent from "../feedComponent/FeedComponent";
 import ProfilePage from "../profilePage/profilePage";
 import UserProfile from "../userPage/userPage";
-
+import './homePage.css'
 let userData = require("../../assets/data/users.json");
 let postData = require("../../assets/data/posts.json");
 
 const Homepage = (props: any) => {
     const [switchParam, setSwitchParam] = useState<string>("home");
     const [user, setUser] = useState<number>(-1);
-    
+
     const renderSwitch = (param: string) => {
-        switch(param) {
+        switch (param) {
             case "home":
-                return <FeedComponent userData={userData} postData={postData} uid={props.uid} paramCallback={setSwitchParam} userCallback={setUser}/>;
+                return <FeedComponent userData={userData} postData={postData} uid={props.uid} paramCallback={setSwitchParam} userCallback={setUser} />;
             case "profile":
-                return <ProfilePage user={userData[props.uid]} postData={postData} uid={props.uid}/>;
+                return <ProfilePage user={userData[props.uid]} postData={postData} uid={props.uid} />;
             case "user":
-                return <UserProfile vid={props.uid} fid={user} user={userData[user]} postData={props.postData} uid={user}/>;
+                return <UserProfile vid={props.uid} fid={user} user={userData[user]} postData={props.postData} uid={user} />;
             default:
-                return <FeedComponent userData={userData} postData={postData} uid={props.uid}/>;
+                return <FeedComponent userData={userData} postData={postData} uid={props.uid} />;
         };
     };
-    return(
+    return (
         <>
             <Navbar bg="light" expand="lg">
-                <Container>
+                <Container fluid>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav 
-                        className="me-auto"
-                        defaultActiveKey={switchParam}
-                        onSelect={(selectedKey) => setSwitchParam(`${selectedKey}`)}
-                    >
-                            <Nav.Link style={{color: "#332FD0"}} eventKey="home">Home</Nav.Link>
-                            <Nav.Link style={{color: "#332FD0"}} eventKey="chats">Messages</Nav.Link>
-                            <Nav.Link style={{color: "#332FD0"}} eventKey="profile">Profile</Nav.Link>
-                            <Nav.Link style={{color: "#332FD0"}} eventKey="explore"> Explore </Nav.Link>
-                    </Nav>
-                    <Button 
-                        onClick={(e)=>{
+                        <Nav
+                            className="me-auto"
+                            defaultActiveKey={switchParam}
+                            onSelect={(selectedKey) => setSwitchParam(`${selectedKey}`)}
+                        >
+
+
+                            <Nav.Link style={{ color: "#332FD0" }} eventKey="home">Home</Nav.Link>
+                            <Nav.Link style={{ color: "#332FD0" }} eventKey="chats">Messages</Nav.Link>
+                            <Nav.Link style={{ color: "#332FD0" }} eventKey="profile">Profile</Nav.Link>
+                            <Nav.Link style={{ color: "#332FD0" }} eventKey="explore"> Explore </Nav.Link>
+
+                        </Nav>
+                        <Button
+                            onClick={(e) => {
                                 e.preventDefault();
                                 auth.signOut();
                             }}
-                    > 
-                        Signout 
-                    </Button>
+                        >
+                            Signout
+                        </Button>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
