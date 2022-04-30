1ee874704321fb5659d79acdd77103962a3d61e9
A-N-uraag <marapallyanuraag@gmail.com>
2022-04-30 05:58:00 +0530

    Resolved Merge Losses

--- frontend/src/components/userPage/userPage.tsx
+++ frontend/src/components/userPage/userPage.tsx
@@ -47,6 +47,24 @@ const UserProfilePage = () => {
         }
     };
 
+    const [show, setShow] = useState(false);
+    const [modalTitle, setModalTitle] = useState<string>("");
+    const [userIds, setUserIds] = useState<string[]>([]);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleFollowerShow = () => {
+        setModalTitle("Followers");
+        setUserIds(user.followers);
+        handleShow();
+    };
+
+    const handleFollowingShow = () => {
+        setModalTitle("Following");
+        setUserIds(user.following);
+        handleShow();
+    };
+
     useEffect(() => {
         const requestOptions = {
             method: 'GET',
@@ -78,6 +96,19 @@ const UserProfilePage = () => {
         return (
             <>
                 <NavComponent />
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title> {modalTitle} </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <UserList userIds={userIds}/>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
                 <Container fluid="true">
                     <Row fluid="true" className="gx-0">
                         <Col xs={0} md={3}></Col>
@@ -109,14 +140,28 @@ const UserProfilePage = () => {
                                 <Card.Footer className="py-0 my-0">
                                     <Container fluid="true" className="gy-0 py-0 my-0">
                                         <Row fluid="true" className="gx-0 gy-0 py-0 my-0">
-                                            <Col fluid="true" className="py-0 my-0">
-                                                <div className="centerAlign py-0 my-0">
+                                            {/* <Col fluid="true" className="py-0 my-0">
+                                                <div className="centerAlign py-0 my-0" onClick={handleFollowerShow}>
                                                     <p style={{display: "inline-block"}} className="text-secondary">Followers: </p><p style={{display: "inline-block"}}>{user.followers ? user.followers.length : 0}</p>
                                                 </div>
                                             </Col>
                                             <Col fluid="true" className="py-0 my-0">
-                                                <div className="centerAlign py-0 my-0">
+                                                <div className="centerAlign py-0 my-0" onClick={handleFollowingShow}>
                                                     <p style={{display: "inline-block"}} className="text-secondary">Following: </p><p style={{display: "inline-block"}}>{user.followers ? user.following.length : 0}</p>
+                                                </div>
+                                            </Col> */}
+                                            <Col fluid="true" className="py-0 my-0">
+                                                <div className="centerAlign py-0 my-0">
+                                                    <Button style={{margin:"3px 0px"}} variant="outline-dark" size="sm" onClick={handleFollowerShow}>
+                                                        <p style={{fontSize:"12px",margin:"0px",display: "inline-block"}} className="text-secondary">Followers&nbsp;&nbsp;</p><p style={{fontSize:"12px",margin:"0px",fontWeight:"semibold",display: "inline-block"}}>{user.followers ? user.followers.length : 0}</p>
+                                                    </Button>
+                                                </div>
+                                            </Col>
+                                            <Col fluid="true" className="py-0 my-0">
+                                                <div className="centerAlign py-0 my-0">
+                                                    <Button style={{margin:"3px 0px"}} variant="outline-dark" size="sm" onClick={handleFollowingShow}>
+                                                        <p style={{fontSize:"12px",margin:"0px",display: "inline-block"}} className="text-secondary">Following&nbsp;&nbsp;</p><p style={{fontSize:"12px",margin:"0px",fontWeight:"semibold",display: "inline-block"}}>{user.following ? user.following.length : 0}</p>
+                                                    </Button>
                                                 </div>
                                             </Col>
                                         </Row>
@@ -144,120 +189,6 @@ const UserProfilePage = () => {
             </>
         );
     }
-
-    const [show, setShow] = useState(false);
-    const [modalTitle, setModalTitle] = useState<string>("");
-    const [userIds, setUserIds] = useState<string[]>([]);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const handleFollowerShow = () => {
-        setModalTitle("Followers");
-        setUserIds(user.followers);
-        handleShow();
-    };
-
-    const handleFollowingShow = () => {
-        setModalTitle("Following");
-        setUserIds(user.following);
-        handleShow();
-    };
-
-    return (
-        <>
-            <NavComponent />
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                <Modal.Title> {modalTitle} </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <UserList userIds={userIds}/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-            <Container fluid="true">
-                <Row fluid="true" className="gx-0">
-                    <Col xs={0} md={3}></Col>
-                    <Col fluid xs={12} md={6}>
-                        <Card>
-                            <Card.Img variant="top" style={imgLoaded ? {width:"30%",margin:"0px auto"} : {display: 'none'}} src={"https://avatars.dicebear.com/api/bottts/"+userId+".svg?colorful=1"}  onLoad={() => setImgLoaded(true)} />
-                            <Card.Body>
-                                <Card.Title>{user.uname}</Card.Title>
-                                <Card.Subtitle> <p className="text-secondary"> {userId} </p> </Card.Subtitle>
-                                <Container fluid="true">
-                                    <Row fluid="true" className="gx-0">
-                                        <Col fluid="true" sm={3}>
-                                            <FontAwesomeIcon size="xs" style={{display: "inline-block"}} icon={faLocationDot} /> <p style={{display: "inline-block"}}>{user.location ? user.location : "Zombieland"}</p>
-                                        </Col>
-                                        <Col fluid="true" sm={3}>
-                                            <FontAwesomeIcon size="xs" style={{display: "inline-block"}} icon={faLink} /> <a href={user.website ? user.website : "https://google.co.in"} style={{display: "inline-block", color: "#332FD0"}}>Website</a>
-                                        </Col>
-                                        <Col fluid="true" sm={3}>
-                                            <FontAwesomeIcon size="xs" style={{display: "inline-block"}} icon={faCakeCandles} /> <p style={{display: "inline-block"}}>{Moment(user.dob).format('LL')}</p>
-                                        </Col>
-                                        <Col fluid="true" sm={3}>
-                                            <FontAwesomeIcon size="xs" style={{display: "inline-block"}} icon={faCalendarDays} /> <p style={{display: "inline-block"}}>{Moment(user.createdDate).format('LL')}</p>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                                {(auth.currentUser === null) ? null : <Button variant={following ? "dark" : "outline-dark"} size="sm" onClick={handleEdge}>{following ? "Unfollow" : "Follow"}</Button>}
-                                
-                            </Card.Body>
-                            <Card.Footer className="py-0 my-0">
-                                <Container fluid="true" className="gy-0 py-0 my-0">
-                                    <Row fluid="true" className="gx-0 gy-0 py-0 my-0">
-                                        {/* <Col fluid="true" className="py-0 my-0">
-                                            <div className="centerAlign py-0 my-0" onClick={handleFollowerShow}>
-                                                <p style={{display: "inline-block"}} className="text-secondary">Followers: </p><p style={{display: "inline-block"}}>{user.followers ? user.followers.length : 0}</p>
-                                            </div>
-                                        </Col>
-                                        <Col fluid="true" className="py-0 my-0">
-                                            <div className="centerAlign py-0 my-0" onClick={handleFollowingShow}>
-                                                <p style={{display: "inline-block"}} className="text-secondary">Following: </p><p style={{display: "inline-block"}}>{user.followers ? user.following.length : 0}</p>
-                                            </div>
-                                        </Col> */}
-                                        <Col fluid="true" className="py-0 my-0">
-                                            <div className="centerAlign py-0 my-0">
-                                                <Button style={{margin:"3px 0px"}} variant="outline-dark" size="sm" onClick={handleFollowerShow}>
-                                                    <p style={{fontSize:"12px",margin:"0px",display: "inline-block"}} className="text-secondary">Followers&nbsp;&nbsp;</p><p style={{fontSize:"12px",margin:"0px",fontWeight:"semibold",display: "inline-block"}}>{user.followers ? user.followers.length : 0}</p>
-                                                </Button>
-                                            </div>
-                                        </Col>
-                                        <Col fluid="true" className="py-0 my-0">
-                                            <div className="centerAlign py-0 my-0">
-                                                <Button style={{margin:"3px 0px"}} variant="outline-dark" size="sm" onClick={handleFollowingShow}>
-                                                    <p style={{fontSize:"12px",margin:"0px",display: "inline-block"}} className="text-secondary">Following&nbsp;&nbsp;</p><p style={{fontSize:"12px",margin:"0px",fontWeight:"semibold",display: "inline-block"}}>{user.following ? user.following.length : 0}</p>
-                                                </Button>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                    <Col xs={0} md={3}></Col>
-                </Row>
-                <ClimbingBoxLoader color="#332FD0" size={20} css={override} loading={posts==="null"}/>
-                {typeof posts === 'object' && Object.keys(posts).reverse().map((pid: string) => {   
-                    const post:any = posts[pid];
-                    return (
-                        <Row key={pid} fluid="true" className="gx-0">
-                            <Col xs={0} md={3}></Col>
-                            <Col fluid xs={12} md={6}>
-                                <Post content={post.content} uname={post.postedByName} likes={post.likes} comments={post.comments} reposts={post.reposts} pid={pid} fid={post.postedBy} uid={auth.currentUser?.uid}createdDate={post.createdDate} />
-                            </Col>
-                            <Col xs={0} md={3}></Col>
-                        </Row>
-                    );
-                }) }
-                {Object.keys(posts).length===0 && <div style={{marginTop:"10%"}} className="text-secondary centerAlign">No posts yet!<br/>Head to the HomePage and post something!</div>}
-            </Container>
-        </>
-    );
 };
 
 export default UserProfilePage;
